a340e1957e75569e34e723b68d595fbd8e692464
carlosTJunior <carlostjunior517@gmail.com>
2023-08-10 18:45:43 -0300

    chore: add postgres container

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "api_database",
+        "USER": "postgres",
+        "PASSWORD": "supersecret",
+        "HOST": "database"
     }
 }
 
